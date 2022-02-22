4f2d736571bf33c325ed0a7c4ad7119197ed5752
Svetlana Shinkar <svetanek@gmail.com>
2022-02-22 11:13:06 -0500

    sam api poller

--- src/handlers/workflow-prepare-poller-job.ts
+++ src/handlers/workflow-prepare-poller-job.ts
@@ -20,3 +20,62 @@ export const handler = async (event: any) : Promise<PollerJob> => {
         taskStateMap: new TaskStateMap()
     };
 }
+
+//FIRST CALL TO GET METADATA: totalItems and maxPerPage
+// getMetadataAsync = async (url: string) : Promise<RequestMetadata> =>  {
+//     const result = await this.executeRequestAsync({
+//         BaseUrl: url,
+//         StartAt: 0,
+//         Count: 1
+//     });
+
+//     return {
+//         BaseUrl: url,
+//         MaxRecordsPerPage: result.maxRecordsPerPage,
+//         TotalRecordsToBeLoaded: result.totalRecords
+//     };
+// }
+
+// private executeRequestAsync = async (request: PagedRequest) : Promise<any> => {
+//     try {
+//         const result = await axios.get(request.BaseUrl, {
+//             headers: {
+//                 'cache-control': 'no-cache',
+//                 'content-type': 'text/plain'
+//             },
+//             params: {
+//                 'startAt': request.StartAt,
+//                 'count': request.Count
+//             }
+//         });
+
+//         return result.data;
+//     } catch (err) {
+//         console.log('Error', JSON.stringify(err));
+//         // we are considering this error intermittent and will want to retry it
+//         throw retriableErrorFactory.retriablePollerError(err);
+//     }
+// }
+// }
+
+
+// createPollerTasks = (jobId: string, metadata: RequestMetadata): PollerTask[] => {
+//     const ttl = this.calculateTimeToLive();
+//     const tasks: PollerTask[] = [];
+
+//     let index: number = 0;
+//     while (index < metadata.TotalRecordsToBeLoaded) {
+//         tasks.push({
+//             request: {
+//                 StartAt: index,
+//                 Count: metadata.MaxRecordsPerPage,
+//                 BaseUrl: metadata.BaseUrl
+//             },
+//             jobId: jobId,
+//             taskId: uuid.v1(),
+//             ttl: ttl
+//         });
+//         index += metadata.MaxRecordsPerPage;
+//     }
+//     return tasks;
+}
